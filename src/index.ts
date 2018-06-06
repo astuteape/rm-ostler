a980f71e866295a457f481ae164c3c0f955b25d9
Wade <wade@astuteape.com>
2018-06-06 15:39:19 -0700

    Prototype responsive navigation
    This commit contains edits to multiple files to address the layout
    of the main content and build the repsonsive navigation menu.

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,18 @@
 
 import "./styles/index.scss";
 
+// Navigation menu
+const mobileNavButton = document.getElementById("nav-mobile-menu");
+const navPagesLinks = document.getElementById("nav-link-container");
+
+mobileNavButton.addEventListener("click", () => {
+  if (navPagesLinks.classList.contains("nav-mobile-collapsed")) {
+    navPagesLinks.classList.remove("nav-mobile-collapsed");
+  } else {
+    navPagesLinks.classList.add("nav-mobile-collapsed");
+  }
+});
+
 // Copyright date
 let yearString = new Date().getFullYear().toString();
 let copyrightDateContainer = document.getElementById("copyright-date");
