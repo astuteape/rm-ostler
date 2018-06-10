3aec46bba71030029c51ee7e3fc894d24be68bb5
Wade <wade@astuteape.com>
2018-06-09 17:05:10 -0700

    Finish MVP of books page

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@
 import "./styles/index.scss";
 
 // Navigation menu
+const body = document.getElementsByTagName("body");
 const navMobileButton = document.getElementById("nav-mobile-menu");
 const navLinkContainer = document.getElementById("nav-link-wrapper");
 
@@ -11,6 +12,11 @@ navMobileButton.addEventListener("click", () => {
     ? navLinkContainer.classList.remove("nav-is-expanded")
     : navLinkContainer.classList.add("nav-is-expanded");
 });
+
+// Prevent CSS animations on load
+window.onload = () => {
+  body[0].classList.remove("preload");
+};
 
 // Copyright date
 let yearString = new Date().getFullYear().toString();
