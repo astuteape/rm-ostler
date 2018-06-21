6a3c818f93968466e35670b5c64578ff4362637a
Wade <wade@astuteape.com>
2018-06-20 21:38:13 -0700

    Animate mobile nav menu

--- src/index.ts
+++ src/index.ts
@@ -3,22 +3,65 @@
 import "./styles/index.scss";
 
 // Prevent CSS animations on load
+
 window.onload = () => {
+  const body = document.getElementsByTagName("body");
   body[0].classList.remove("is-preloading");
 };
 
 // Navigation menu
-const body = document.getElementsByTagName("body");
-const navMobileButton = document.getElementById("nav-mobile-menu");
-const navLinkContainer = document.getElementById("nav-link-wrapper");
+// Define collapsible animation functions
+const collapseSection = (element: HTMLElement) => {
+  const contentHeight: number = element.scrollHeight;
+  const elementTransition: string = element.style.transition;
 
-navMobileButton.addEventListener("click", () => {
-  navLinkContainer.classList.contains("nav-is-expanded")
-    ? navLinkContainer.classList.remove("nav-is-expanded")
-    : navLinkContainer.classList.add("nav-is-expanded");
-});
+  requestAnimationFrame(() => {
+    // Set element height before collapse frame
+    element.style.height = `${contentHeight}px`;
+    element.style.transition = elementTransition;
+
+    requestAnimationFrame(() => {
+      element.style.height = `${0}px`;
+      element.style.overflow = "hidden";
+    });
+  });
+
+  element.setAttribute("data-collapsed", "true");
+};
+
+const expandSection = (element: HTMLElement) => {
+  const contentHeight: number = element.scrollHeight;
+
+  element.style.height = `${contentHeight}px`;
+  element.setAttribute("data-collapsed", "false");
+};
+
+const toggleCollapse = (
+  eventElement: HTMLElement,
+  animateElement: HTMLElement,
+  event: string
+) => {
+  eventElement.addEventListener(`${event}`, () => {
+    const isCollapsed =
+      animateElement.getAttribute("data-collapsed") === "true";
+
+    if (isCollapsed) {
+      expandSection(animateElement);
+      animateElement.setAttribute("data-collapsed", "false");
+    } else {
+      collapseSection(animateElement);
+    }
+  });
+};
+
+// Call collapsible animation on navigation
+const mobileNavButton: HTMLElement = document.querySelector(
+  "#nav-mobile-button"
+);
+const navLinkWrapper: HTMLElement = document.querySelector("#nav-link-wrapper");
+toggleCollapse(mobileNavButton, navLinkWrapper, "click");
 
 // Copyright date
 let yearString = new Date().getFullYear().toString();
-let copyrightDateContainer = document.getElementById("copyright-date");
-copyrightDateContainer.innerHTML = yearString;
+let copyrightDateWrapper = document.getElementById("copyright-date");
+copyrightDateWrapper.innerHTML = yearString;
