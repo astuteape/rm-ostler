3b1be41bc3e2d4e2e9a152d989e647f783e8346c
Wade <wade@astuteape.com>
2018-07-01 14:22:43 -0700

    Update webpack for production build

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,9 @@
 const path = require("path");
 
 // Styles
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const bourbon = require("bourbon");
 const neat = require("bourbon-neat");
 const StyleLintPlugin = require("stylelint-webpack-plugin");
@@ -33,6 +35,16 @@ module.exports = {
           }
         ]
       }
+    ]
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true // set to true if you want JS source maps
+      }),
+      new OptimizeCSSAssetsPlugin({})
     ]
   },
   plugins: [
